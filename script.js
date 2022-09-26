0b0d27dab9629dce6f146cce0af803d2fe879b54
izzatbekdev <Izzattexnika@gmail.com>
2022-09-26 22:53:27 +0500

    second commit

---
+++ script.js
@@ -0,0 +1,36 @@
+"use strict";
+// 1)
+// let sm=prompt('sm kiriting');
+// console.log(sm/100+ " metr");
+
+// 2)
+// let kg=prompt('kg kiriting');
+// console.log(kg/1000 + " tonna");
+
+//3)
+// let A=prompt('A musbat son kiriting');
+// let B=prompt('B musbat son kiriting');
+// if(A>B){
+// console.log(A/B);
+// }
+
+// 4)
+// let n=prompt('Ikki xonali son kiriting');
+// console.log(Math.trunc(n/10)+ " o\'nlik xonasidagi raqam" );
+// console.log(n%10+ ' birlik xonadagi raqam');
+
+// 5)
+// let a=prompt('Ikki xonali son kiriting');
+// console.log(Math.trunc(a/10)+(a%10)+(` raqamlar yig'indisi`));
+
+//6)
+// const total=prompt("Ikki xonali son kiriting");
+// console.log(Math.trunc(total/10)*(total%10)+(` raqamlar ko'paytirmasi`));
+
+//7)-----
+
+//8)
+const time=prompt('N soniya kiriting');
+console.log((time/60)+(' minut;'), (time/3600)+(' soat'));
+
+
